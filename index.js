fa4dacc947fa57e26f07a9b2ad696970c11e7060
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-27 18:43:42 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-import fresh from "@unction/fresh"
-import attach from "@unction/attach"
-import reduceWithValueKey from "@unction/reducewithvaluekey"
-
-import type {PredicateFunctionType} from "types"
-import type {UnaryFunctionType} from "types"
-import type {EnumerableType} from "types"
-
-export default function selectByValue (predicate: PredicateFunctionType): UnaryFunctionType {
-  return function selectByValuePredicate (enumerable: EnumerableType): EnumerableType {
+import fresh from "@unction/fresh";
+import attach from "@unction/attach";
+import reduceWithValueKey from "@unction/reducewithvaluekey";
+export default function selectByValue(predicate) {
+  return function selectByValuePredicate(enumerable) {
     if (enumerable.filter) {
-      return enumerable.filter(predicate)
+      return enumerable.filter(predicate);
     }
 
-    return reduceWithValueKey(
-      (accumulated: EnumerableType): UnaryFunctionType =>
-        (value: mixed): UnaryFunctionType =>
-          (key: KeyType): EnumerableType => {
-            if (predicate(value)) {
-              return attach(key)(value)(accumulated)
-            }
+    return reduceWithValueKey(accumulated => value => key => {
+      if (predicate(value)) {
+        return attach(key)(value)(accumulated);
+      }
 
-            return accumulated
-          }
-    )(
-      fresh(enumerable)
-    )(
-      enumerable
-    )
-  }
+      return accumulated;
+    })(fresh(enumerable))(enumerable);
+  };
 }
