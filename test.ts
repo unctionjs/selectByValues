613dffc62de3df483879c98f8ff38afe79b5ec59
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 13:08:12 -0700

    Continuing to setup testing

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@
 import {from} from "most";
 import streamSatisfies from "@unction/streamsatisfies";
 
-import selectByValue from "./index.ts";
+import selectByValue from "./index";
 
 const isOdd = (value) => value % 2 !== 0;
 
